9e1136f4063bfa1e2c194058324a576f16063f17
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-04-07 19:41:42 +0300

    Добавлены динамические роуты

--- my_page/horoscope/views.py
+++ my_page/horoscope/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
+zodiac_dict = {
+    'aries': 'Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).',
+    'taurus': 'Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).',
+    'gemini': 'Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).',
+    'cancer': 'Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).',
+    'leo': ' Лев - <i>пятый знак зодиака</i>, солнце (с 23 июля по 21 августа).',
+    'virgo': 'Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).',
+    'libra': 'Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
+    'scorpio': 'Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).',
+    'sagittarius': 'Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).',
+    'capricorn': 'Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).',
+    'aquarius': 'Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).',
+    'pisces': 'Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).',
+}
 
 
-# Create your views here.
-def leo(request):
-    return HttpResponse('Лео')
+def zod(request, zodiak: str):
+    return HttpResponse(f'{zodiac_dict.get(zodiak, "Такого знака не существует")}')
 
-def scorpio(request):
-    return HttpResponse('Скорпион')
+
+def by_number(request, zodiak: int):
+    z = list(zodiac_dict.keys())
+    return HttpResponse(f'{zodiac_dict.get(z[zodiak - 1], "Такого знака не существует")}')

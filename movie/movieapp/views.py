13ff97387a697e3ac5f6ef729283785d195522b1
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-05-09 21:36:56 +0300

    work with aggregate

--- movie/movieapp/views.py
+++ movie/movieapp/views.py
@@ -1,4 +1,4 @@
-from django.db.models import F
+from django.db.models import F, Sum, Avg, Min, Max
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -7,7 +7,12 @@ from movieapp.models import Movie
 
 def main_page(request):
     movies = Movie.objects.order_by(F("year").asc(nulls_last=True), 'rating')
-    return render(request, 'movieapp/all_movies.html', {'movies': movies})
+    agg = movies.aggregate(budget=Avg('budget'), rating_max=Max('rating'), rating_min=Min('rating'))
+    count_movies = movies.count()
+    return render(request, 'movieapp/all_movies.html', {'movies': movies,
+                                                        'agg': agg,
+                                                        "count_movies": count_movies})
+
 
 
 def show_one_movie(request, slug_movie):

21c18990239418d30fbde83f54702153e5888b2c
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-05-11 21:58:30 +0300

    Тестирование моделей

--- movie/movieapp/models.py
+++ movie/movieapp/models.py
@@ -7,7 +7,7 @@ class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True)
-    budget = models.IntegerField(default=100000)
+    budget = models.IntegerField(default=1000000)
     slug = models.SlugField()
 
     def save(self, *args, **kwargs):
@@ -15,7 +15,7 @@ class Movie(models.Model):
         super(Movie, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f'{self.name}: {self.rating}'
+        return f'{self.name} - {self.rating}%'
 
     def get_absolute_url(self):
         return reverse('one_movie', args=[self.slug])

794350ddc1797dfb20949b2b5ce9a51671086afd
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-05-21 14:25:54 +0300

    Admin panel

--- movie/movieapp/admin.py
+++ movie/movieapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from movieapp.models import Movie
 # Register your models here.
+
+
+admin.site.site_header = 'Наша админка'
+
+admin.site.index_title = 'Фильмы'
+
+class MovieAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'rating', 'year']
+
+
+admin.site.register(Movie, MovieAdmin)
